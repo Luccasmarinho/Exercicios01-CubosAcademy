3b3e102553948ef0f65e7af031487b804dc361a2
Luccasmarinho <luccas88oliveira@gmail.com>
2023-08-25 13:45:19 -0300

    Resposta exercício 05

---
+++ 05/index.js
@@ -0,0 +1,20 @@
+const x1 = 5;
+const x2 = 1;
+const y1 = 1;
+const y2 = 1;
+
+let ponto1 = x2 - x1;
+let ponto2 = y2 - y1;
+
+ponto1 = ponto1 ** 2;
+ponto2 = ponto2 ** 2;
+
+let d = Math.sqrt(ponto1 + ponto2);
+console.log(`A distância entre os dois pontos dados é de: ${d}`);
+
+
+
+
+
+
+
